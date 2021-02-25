d027b2832dfbd8adf8649d73625d66acd040718b
Veronika Markova <veronica.mkv11@gmail.com>
2021-02-25 19:56:46 +0400

    SwiftUI tutorial.

---
+++ NBA Player Fun/NBA_Player_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Player_FunApp.swift
+//  NBA Player Fun
+//
+//  Created by Veronica Markova on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Player_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
